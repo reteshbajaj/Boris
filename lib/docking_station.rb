3a5577448034ebc2988392cb3149c0d7779467f8
Retesh <retesh.bajaj@gmail.com>
2015-07-22 12:45:01 +0100

    Post-Challenge 1 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,14 @@
 class DockingStation
 
-  def release_bike
-  	bike = Bike.new
+  def dock(bmx)
+  bmx = Bike.new ##Until we added this line (LAST) out feature test failed but the unit test passed
+  @bike = bmx ##gives value bike when dock is called on argument bike
+### THIS DEFINITION IS ASSIGNING THE ARGUMENT TO THE INSTANCE VARIABLE
   end
 
+  def release_bike
+  fail 'No bikes available' unless @bike #IE WHEN THE INSTANCE VARIABLE @BIKE IS NOT NIL
+  @bike ##ALTERNATE - Bike.new also works here
+  end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,10 +2,21 @@ require "docking_station"
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
+  it "can be docked" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
 
- it "produces a bike and the bike works" do
- 	instance = Bike.new
-  expect(Bike.new).to be_working ##can use instance or Bike.new in brackets here
- end
+  describe '#release_bike' do
+
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+ it 'releases working bikes' do
+    subject.dock Bike.new ##WARNING - this allows the unit test to pass because it assumes that the argument that dock takes when called is a member of that class - this is not defined in the code. Until defined, the feature test fails!
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
 
